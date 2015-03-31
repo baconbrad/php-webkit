f41adc7d811962eb2cbd091e409b3eb162fb7109
Brad Metcalf <brad@localabstract.com/>
2015-03-30 19:03:47 -0500

    Added more control of php-webkit through package.json

--- php-webkit/main.js
+++ php-webkit/main.js
@@ -4,21 +4,42 @@ var http = require('http');
 var php = require('node-php');
 var express = require('express');
 var app = express();
+var os = require('os');
+var os = os.platform();
 
 win.title = gui.App.manifest.name;
 
+var bin = gui.App.manifest.phpwebkit.bin;
+if(bin === undefined || bin == "") {
+	if(os == 'win32' || os == 'win64') {
+		bin = './php/php-cgi.exe';
+	} else if(os == 'darwin') {
+		bin = './php/php-cgi';
+	} else if(os == 'linux') {
+		bin = './php/php-cgi';
+	} else {
+		bin = 'php-cgi';
+	}
+}
+
+var path = gui.App.manifest.phpwebkit.path;
+if(path === undefined || path == "") {
+	path = './application';
+}
+
 var host = gui.App.manifest.phpwebkit.host;
-if(host === undefined) {
+if(host === undefined || host == "") {
 	host = '127.0.0.1';
 }
 
 var port = gui.App.manifest.phpwebkit.port;
-if(port === undefined) {
+if(port === undefined || port == "") {
 	port = 9090;
 }
 
 var phpinfo = {
-	"path": "./application",
+	"path": path,
+	"bin": bin,
 	"host": host,
 	"port": port,
 	"arguments": gui.App.argv,
